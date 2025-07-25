80fca378a190b109b66609c2158dd8e13cc29e3a
Jacob Baker <jbaker23@students.northweststate.edu>
2025-07-25 11:29:37 -0400

    Assignment Revised

--- computer.py
+++ computer.py
@@ -1,10 +1,33 @@
-#My name is Jacob Baker and this is Chapter 14 Lab 2 which is being done on July 12
+#My name is Jacob Baker and this is Chapter 14 Lab 2 which is being done on July 12 and being revised on July 24
+from dataclasses import dataclass
+@dataclass
+
 class Computer:
+    def __init__(self, cpu_speed, ram_size, drive_size):
+        self.__cpu_speed = cpu_speed
+        self.__ram_size = ram_size
+        self.__drive_size = drive_size
 
-    def __init__(self,cpu_speed,ram_size,drive_size):
-        self.cpu_speed = cpu_speed
-        self.ram_size = ram_size
-        self.drive_size = drive_size
+    @property
+    def cpu_speed(self):
+        return self.__cpu_speed
 
-    def __str__(self):
-        return f'Computer(_Computer__cpu_speed={self.cpu_speed}, _Computer__ram_size={self.ram_size}, _Computer__drive_size={self.drive_size})'
+    @cpu_speed.setter
+    def cpu_speed(self, value):
+        self.__cpu_speed = value
+
+    @property
+    def ram_size(self):
+        return self.__ram_size
+
+    @ram_size.setter
+    def ram_size(self, value):
+        self.__ram_size = value
+
+    @property
+    def drive_size(self):
+        return self.__drive_size
+
+    @drive_size.setter
+    def drive_size(self, value):
+        self.__drive_size = value
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#My name is Jacob Baker and this is Chapter 14 Lab 2 which is being done on July 12
+#My name is Jacob Baker and this is Chapter 14 Lab 2 which is being done on July 12. I am revising on July 25.
 import computer
 
-computer1 = computer.Computer(20,16,2000)
-computer2 = computer.Computer(50,32,5000)
+computer1 = computer.Computer(20, 16, 2000)
+computer2 = computer.Computer(50, 32, 5000)
 
-print(computer1)
-print(computer2)
+print(f"Computer1(_Computer__cpu_speed={computer1.cpu_speed}, _Computer__ram_size={computer1.ram_size}, _Computer__drive_size={computer1.drive_size})")
+print(f"Computer2(_Computer__cpu_speed={computer2.cpu_speed}, _Computer__ram_size={computer2.ram_size}, _Computer__drive_size={computer2.drive_size})")
 
